4d13e01b16ce0776172de62251dbeef8ee7c6a9f
IgorYarutkin <igor.yarutkin@gmail.com>
2017-03-16 21:55:14 +0300

    :UP:

--- app/blocks/map/map.js
+++ app/blocks/map/map.js
@@ -1,9 +1,9 @@
 'use strict';
 
 window.onload = function () {
-    // Создает экземпляр карты и привязывает его к созданному контейнеру
-    const map = new YMaps.Map(document.getElementById('YMapsID'));
+	// Создает экземпляр карты и привязывает его к созданному контейнеру
+	const map = new window.YMaps.Map(document.getElementById('YMapsID'));
 
-    // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
-    map.setCenter(new YMaps.GeoPoint(59.95, 30.29), 10);
+	// Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
+	map.setCenter(new window.YMaps.GeoPoint(59.95, 30.29), 10);
 };
